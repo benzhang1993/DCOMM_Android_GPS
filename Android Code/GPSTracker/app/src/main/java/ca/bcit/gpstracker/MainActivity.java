c222d35a870f713e608103821cae34b7d70e42fa
choyjef <choyjef@gmail.com>
2019-03-26 10:18:51 -0700

    added connected toast for connection feedback

--- Android Code/GPSTracker/app/src/main/java/ca/bcit/gpstracker/MainActivity.java
+++ Android Code/GPSTracker/app/src/main/java/ca/bcit/gpstracker/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -70,6 +71,9 @@ public class MainActivity extends AppCompatActivity {
         Button sendButton = (Button)findViewById(R.id.send_button);
         sendButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+                Toast toast = Toast.makeText(MainActivity.this, "Connected", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.TOP, 0 ,0);
+                toast.show();
                 getDeviceLocation();
                 setLocationJSON();
                 EditText ipField = (EditText) findViewById(R.id.ip_field);
